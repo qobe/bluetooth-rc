e00e638eeafe1f6c2416e293043da659ecdb2f5e
qobe <max.kobe@outlook.com>
2015-02-04 19:52:15 -0500

    Finishing GUI
    GUI is finished. Working on Bluetooth comm

--- Blueknobs/app/src/main/java/kobe/blueknobs/BluetoothHandler.java
+++ Blueknobs/app/src/main/java/kobe/blueknobs/BluetoothHandler.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Set;
 import java.util.UUID;
 
 /**
@@ -16,11 +15,9 @@ import java.util.UUID;
  */
 public class BluetoothHandler {
     private BluetoothAdapter mBlueAdapter;
-    private BluetoothDevice mBlueDevice;
-    private BluetoothSocket mBlueSocket;
-    private OutputStream mBlueOut;
-    private InputStream mBlueIn;
-    private final String MY_UUID = "1234";
+    private ConnectThread mConnectThread;
+
+    static final String MY_UUID = "00001101-0000-1000-8000-00805F9B34FB";
     private ArrayList<BluetoothDevice> mPairedDevices;
 
     public BluetoothHandler(BluetoothAdapter ba){
@@ -32,44 +29,76 @@ public class BluetoothHandler {
     }
 
     public void open(BluetoothDevice dev){
-        mBlueDevice = dev;
-        try {
-            mBlueSocket = dev.createRfcommSocketToServiceRecord(UUID.fromString(MY_UUID));
-        } catch (IOException e) {
-            e.printStackTrace();
+        mBlueAdapter.cancelDiscovery();
+        mConnectThread =  new ConnectThread(dev);
+    }
+
+
+    public void send(String data) {
+        byte msg[] = new byte[4];
+        //convert string into byte array
+        //T or A is 2 bytes.
+        for (int i = 0; i < data.length(); i++) {
+            msg[i] = (byte) data.charAt(i);
         }
     }
-
-    public void connect(){
-        mBlueAdapter.cancelDiscovery();
-        //attempt to open connection. if failed close socket
-        new Thread(new Runnable(){
-            public void run(){
-                try {
-                    mBlueSocket.connect();
-                } catch (IOException connectException) {
-                    try {
-                        mBlueSocket.close();
-                    } catch (IOException e) {}
-                }
-            }
-        }).start();
-    }
-
-    public void send(byte[] msg){
-        try {
-            mBlueOut.write(msg);
-        } catch (IOException e) {}
-    }
-
     public void cancel(){
-        try {
-            mBlueSocket.close();
-        } catch (IOException e) {}
+        mConnectThread.close();
     }
+
 
     public ArrayList<BluetoothDevice> getPairedDevices(){
         return mPairedDevices;
+    }
+
+    //attempt with private class.
+    private class ConnectThread extends Thread{
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+        private final OutputStream mBlueOut;
+        private final InputStream mBlueIn;
+
+        private ConnectThread(BluetoothDevice dev){
+            mmDevice = dev;
+            BluetoothSocket tmp = null;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get a BluetoothSocket to connect with the given BluetoothDevice
+            try {
+                // MY_UUID is the app's UUID string, also used by the server code
+                tmp = dev.createRfcommSocketToServiceRecord(UUID.fromString(MY_UUID));
+                tmpOut = tmp.getOutputStream();
+                tmpIn = tmp.getInputStream();
+            } catch (IOException e) { }
+            mmSocket = tmp;
+            mBlueOut = tmpOut;
+            mBlueIn = tmpIn;
+        }
+
+        public void run(){
+            try {
+                mmSocket.connect();
+            } catch (IOException connectException) {
+                try {
+                   mmSocket.close();
+                } catch (IOException e) {}
+            }
+        }
+
+        private void write(byte[] bytes){
+            try {
+                mBlueOut.write(bytes);
+            } catch (IOException e) { }
+        }
+
+        private void close(){
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
 
--- Blueknobs/app/src/main/java/kobe/blueknobs/activity/MainActivity.java
+++ Blueknobs/app/src/main/java/kobe/blueknobs/activity/MainActivity.java
@@ -4,68 +4,68 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.PopupMenu;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Set;
-import java.util.UUID;
+import android.widget.Toast;
 
 import kobe.blueknobs.BluetoothHandler;
 import kobe.blueknobs.R;
 
 public class MainActivity extends ActionBarActivity {
 
-    private static final int REQUEST_ENABLE_BT = 87; //Request code for activity result
     private static final int CONNECTION_STATE_CHANGED = 8701; //request code for activity result to. check if dropped connection
-    private static final int BLUETOOTH_MENU = 9; //group ID of popup menu list bluetooth devices
-    private  UUID MY_UUID;
+   // private static final int BLUETOOTH_MENU = 9; //group ID of popup menu list bluetooth devices
     private BluetoothDevice gSelectedDevice;
     private BluetoothHandler mBlueHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        //startActivityForResult(new Intent(BluetoothAdapter.ACTION_CONNECTION_STATE_CHANGED), CONNECTION_STATE_CHANGED);
 
         //Enable Connect Button
         TextView cb = (TextView)findViewById(R.id.connectButton);
         cb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Create popup menu for connection options
-                PopupMenu popupMenu = new PopupMenu(MainActivity.this, MainActivity.this.findViewById(R.id.connectButton));
-                 //popupMenu.getMenuInflater().inflate(R.menu.popupmenu, popupMenu.getMenu());
                 mBlueHandler = new BluetoothHandler(BluetoothAdapter.getDefaultAdapter());
-                //Intent connState = new Intent(BluetoothAdapter.ACTION_CONNECTION_STATE_CHANGED);
-                //startActivityForResult(connState, CONNECTION_STATE_CHANGED);
-
+                //Create popup menu for connection options, anchor it to connectbutton
+                PopupMenu popupMenu = new PopupMenu(MainActivity.this, v);
                 //populate popup menu with paired devices
                 int i = 0;
                 for(BluetoothDevice dev : mBlueHandler.getPairedDevices()){
                     //getName and getAddress from each device and list
-                    popupMenu.getMenu().add(BLUETOOTH_MENU, i, i,
-                            String.format("dev: %s%nmac: %s", dev.getName(), dev.getAddress()));
+                    popupMenu.getMenu().add(Menu.NONE, i, Menu.NONE,
+                            String.format("dev: %s", dev.getName()));
                     i++;
                 }
-                popupMenu.show();
+                popupMenu.getMenuInflater().inflate(R.menu.popupmenu, popupMenu.getMenu());
                 popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                     @Override
                     public boolean onMenuItemClick(MenuItem item) {
                         //if connect to bluetooth chosen return true
                         mBlueHandler.open(mBlueHandler.getPairedDevices().get(item.getItemId()));
-                        mBlueHandler.connect();
                         return true;
                     }
                 });
+                popupMenu.show();
+                new RCController(mBlueHandler);
             }
         });
+        //initiate control seekbars
+
+
+
     }
 
     @Override
@@ -73,6 +73,21 @@ public class MainActivity extends ActionBarActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if((resultCode == RESULT_OK) && (requestCode == CONNECTION_STATE_CHANGED)) {
             //get extras
+            int state = data.getIntExtra(BluetoothAdapter.EXTRA_CONNECTION_STATE, -1);
+            TextView tv = (TextView)findViewById(R.id.connectButton);
+            switch (state){
+            //state connected: make button blue
+                case BluetoothAdapter.STATE_CONNECTED:
+                    tv.setBackgroundColor(Color.BLUE);
+                    Toast.makeText(MainActivity.this, "Connected..", Toast.LENGTH_SHORT);
+                    break;
+            //state disconnected: make button clear
+                case BluetoothAdapter.STATE_DISCONNECTED:
+                    tv.setBackgroundColor(Color.YELLOW);
+                    Toast.makeText(MainActivity.this, "Disconnected..", Toast.LENGTH_SHORT);
+                    break;
+                default: break;
+            }
         }
     }
 
@@ -101,7 +116,59 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public void onStop(){
         super.onStop();
-        mBlueHandler.cancel();
+       // mBlueHandler.cancel();
     }
 
+
+    private class RCController{
+        //load seek bars for throttle and steering
+        private SeekBar throttle; //send value between 0-255
+        private SeekBar steering; //send value between 60-120
+        private BluetoothHandler blue;
+
+        private RCController(BluetoothHandler bh){
+            blue = bh;
+            throttle = (SeekBar)findViewById(R.id.accelerateBar);
+            steering = (SeekBar)findViewById(R.id.steeringBar);
+            throttle.setMax(255);
+            steering.setMax(60);
+
+            //create seekbar listener for events
+            SeekBar.OnSeekBarChangeListener sbl = new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    if(fromUser){
+                        String str = Integer.toString(progress);
+                        switch(seekBar.getId()){
+                            case R.id.accelerateBar:
+                                //accelerate(progress);
+                                ((TextView)findViewById(R.id.throttleView)).setText("Load: "+str);
+                                break;
+                            case R.id.steeringBar:
+                                //turn(progress);
+                                ((TextView)findViewById(R.id.steeringView)).setText("Angle: "+str);
+                                break;
+                            default: break;
+                        }
+                    }
+                }
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {}
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {}
+            };
+            throttle.setOnSeekBarChangeListener(sbl);
+            steering.setOnSeekBarChangeListener(sbl);
+        }
+
+        private void turn(int angle){
+            angle = angle + 60;
+            blue.send("T"+Integer.toString(angle));
+        }
+
+        private void accelerate(int load){
+            blue.send("A"+Integer.toString(load));
+        }
+    }
 }
